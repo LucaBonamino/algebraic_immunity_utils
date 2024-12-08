4dfdea696d7b8ffd1cb83a1c7685fbb7530b9adb
programmingAthlete <luca.bonamino@hotmail.com>
2024-12-08 23:30:30 +0100

    test

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import algebraic_immunity_utils
 
-print(algebraic_immunity_utils.sum_as_string(1,2))
 
-print(algebraic_immunity_utils.echelon_form_last_row([[1, 0, 0], [1,1,0]]))
 
 m = algebraic_immunity_utils.Matrix([[1,1], [1,0]])
-m1 = m.echf_2()[0]
+m1 = m.echelon_form_last_row()[0]
 print(m1)
 print(m1.rank())
 
 
 m = algebraic_immunity_utils.Matrix([[1,1], [1,1]])
-m1 = m.echf_2()[0]
+m1 = m.echelon_form_last_row()[0]
 print(m1)
 print(m1.rank())
 
