b4da0482bb110e5ec7fc42072d59574af2d9b0b6
programmingAthlete <luca.bonamino@hotmail.com>
2025-01-24 23:27:40 +0100

    some small unit tests

--- tests/main.py
+++ tests/main.py
@@ -3,16 +3,6 @@ import algebraic_immunity_utils
 
 
 m = algebraic_immunity_utils.Matrix([[1,1], [1,0]])
-m1 = m.echelon_form_last_row()[0]
-print(m1)
-print(m1.rank())
-
-
-m = algebraic_immunity_utils.Matrix([[1,1], [1,1]])
-m1 = m.echelon_form_last_row()[0]
-print(m1)
-print(m1.rank())
-
 
 m.append_row([1,0])
 print(m)
